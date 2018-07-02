7e56652fd79416c23e44b931edcc6b484ce54d8a
Sebastianku <Sebastianku46@gmail.com>
2018-07-02 19:52:48 +0800

    homework2-1

---
+++ homework2.py
@@ -0,0 +1,23 @@
+#homework2
+
+#四則運算
+n1=int(input("enter a Number"))
+d1=input("填入運算： ＋, -, *, / ")
+n2=int(input("enter a Number"))
+
+if d1=="+":
+    result=n1+n2
+elif d1=="-":
+    result=n1-n2
+elif d1=="*":
+    result=n1*n2
+elif d1=="/":
+    result=n1/n2
+
+print(int(result))
+
+#開根號
+
+
+
+#9X9列表
